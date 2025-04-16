80433bc52ec7a70cd9ed98723c5ace92643628e6
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-04-16 18:05:39 +0300

    Upd: Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Header() {
+export default function Header() {
+  
   const handleCreateTask = () => {
     console.log('Открыть форму создания задачи');
   };
@@ -11,18 +12,15 @@ function Header() {
       
       <Toolbar>
         
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Avito Tracker Lite
-        </Typography>
+        <Button color="inherit" component={Link} to="/issues">
+          Все задачи
+        </Button>
         
         <Button color="inherit" component={Link} to="/boards">
           Проекты
         </Button>
         
-        <Button color="inherit" component={Link} to="/issues">
-          Все задачи
-        </Button>
-        
+        <div style={{ flexGrow: 1 }} /> {/* Пространство, чтобы кнопка "Создать задачу" была справа */}
         <Button color="inherit" onClick={handleCreateTask}>
           Создать задачу
         </Button>
@@ -32,5 +30,3 @@ function Header() {
     </AppBar>
   );
 }
-
-export default Header;
